0457f034f74ad997a1361d679eb5fcc9132fa56d
choijaewoong <jwchoi0129@gmail.com>
2017-03-06 14:31:38 +0900

    axios를 이용한 비동기 요청 기본 기능 구현

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,7 +3,7 @@ import './Post.css';
 import { CommentList } from '../';
 
 const Post = () => (
-    <div className = "post">
+    <div className="post">
         <h1>Title</h1>
         <p>
             Body
--- src/containers/PostContainer/PostContainer.js
+++ src/containers/PostContainer/PostContainer.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 import { PostWrapper,  Navigate, Post } from '../../components';
+import * as service from '../../services/post';
 
 class PostContainer extends Component {
+
+    componentDidMount() {
+        this.fetchPostInfo(1);
+    }
+
+    /*
+    async function fetchPostInfo(postId) {
+        const post = await service.getPost(postId);
+        console.log(post);
+        const comments = await service.getComments(postId);
+        console.log(comments);
+    }
+    */
+    fetchPostInfo = async(postId) => {
+        // const post = await service.getPost(postId);
+        // console.log(post);
+        // const comments = await service.getComments(postId);
+        // console.log(comments);
+
+        // 두가지일을 동시에 진행하고 싶은 경우 Promise.all을 사용
+        const info = await Promise.all([
+            service.getPost(postId),
+            service.getComments(postId)
+        ]);
+
+        console.log(info);
+    }
+
     render() {
         return (
             <PostWrapper>
