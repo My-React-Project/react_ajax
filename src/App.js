1381dae1eae9c19f2ff01b21de0716799c8f181f
choijaewoong <jwchoi0129@gmail.com>
2017-03-06 10:26:08 +0900

    Header Component 구성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components';
+
 
 class App extends Component {
   render() {
     return (
-      <div>        
+      <div>
+          <Header/>
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <div className="Header">
+//                 POSTS
+//             </div>
+//         );
+//     }
+// }
+const Header = () => (
+    <div className="Header">
+        POSTS
+    </div>
+)
+
+export default Header;
